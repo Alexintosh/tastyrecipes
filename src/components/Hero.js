96355e1a1f9aa164a324e284be24d10053eef551
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-11-11 13:06:13 -0500

    hero margins

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
- 
 
 const Container = styled.div`
   display: flex;
@@ -10,42 +9,42 @@ const Container = styled.div`
   background-color: var(--white);
   color: var(--white);
   padding: 4%;
-  margin-top: 20px;
+  margin-top: 50px;
 
   @media (max-width: 768px) {
     flex-direction: column-reverse;
     justify-content: center;
     padding: 4%;
-  margin-top: 20px;  }
+    margin-top: 10%;
+  }
 `;
 
-
 const Text = styled.div`
-margin: 20px;
+  margin: 20px;
 
   @media (max-width: 768px) {
   }
 `;
 
 const Higlight = styled.span`
-font-weight: 700;
-font-size: 3.2rem;
-line-height: 3.2rem;
-color: var(--black);
-background: #fe0;
--webkit-box-shadow: 1rem 0 0 #fe0, -1rem 0 0 #fe0;
-box-shadow: 1rem 0 0 #fe0, -1rem 0 0 #fe0;
+  font-weight: 700;
+  font-size: 3.2rem;
+  line-height: 3.2rem;
+  color: var(--black);
+  background: #fe0;
+  -webkit-box-shadow: 1rem 0 0 #fe0, -1rem 0 0 #fe0;
+  box-shadow: 1rem 0 0 #fe0, -1rem 0 0 #fe0;
 
   @media (max-width: 768px) {
   }
 `;
 
 const Image = styled.div`
-background-color: #ccc;
-background-image: url('https://www.maxim.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_480/MTQ0MjU4NjU3OTY4NzI3OTU5/giphygif.webp');
-background-size: cover;
-height: 340px;
-width: 100%;
+  background-color: #ccc;
+  background-image: url("https://www.maxim.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_480/MTQ0MjU4NjU3OTY4NzI3OTU5/giphygif.webp");
+  background-size: cover;
+  height: 340px;
+  width: 100%;
 
   @media (max-width: 768px) {
   }
@@ -55,10 +54,10 @@ function Hero2() {
   return (
     <Container>
       <Text>
-          <Higlight>
+        <Higlight>
           Put some salty sprinkles on your Crypto. We'll do the rest.
-          </Higlight>
-        </Text>
+        </Higlight>
+      </Text>
       <Image />
     </Container>
   );
